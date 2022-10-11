42d3eeab3906e15fcc3f6a86838e65fd2df0174d
Zinostick <99473643+Zinostick@users.noreply.github.com>
2022-10-11 16:12:08 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-"""
-User creation class
-"""
-from models.base_model import BaseModel
+"""This module defines a class User"""
+from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy.orm import relationship
+
+STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
-class User(BaseModel):
-    """Defines attributes for user creation"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
+    if STORAGE == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade="all, delete-orphan")
+        reviews = relationship('Review', backref='user',
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
