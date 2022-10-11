ecd7c454483e95da853835b09dec69a61a68c5d2
Zinostick <99473643+Zinostick@users.noreply.github.com>
2022-10-11 16:06:23 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-"""
-Defines city
-"""
-from models.base_model import BaseModel
+""" City Module instances cities with ids """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy.orm import relationship
+
+STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
-class City(BaseModel):
-    """defines city to look for"""
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    if STORAGE == "db":
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship('Place',
+                              backref='cities',
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
